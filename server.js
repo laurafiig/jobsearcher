b78db74b330ce8b9d7950617374b5487f140ac6a
Laura Fiig <Laura Fiig>
2017-01-19 21:13:48 -0600

    heroku path added to server

--- server.js
+++ server.js
@@ -24,7 +24,8 @@ app.use(express.static("./public"));
 // -------------------------------------------------
 
 // MongoDB Configuration configuration (Change this URL to your own DB)
-mongoose.connect("#");
+mongoose.connect("mongodb://localhost/jobsearcher");
+//mongoose.connect("mongodb://heroku_n9z61jwb:g70uj5oi9kcut1g19ru8ur1ori@ds117899.mlab.com:17899/heroku_n9z61jwb");
 var db = mongoose.connection;
 
 db.on("error", function(err) {
@@ -44,11 +45,11 @@ app.get("/", function(req, res) {
 
 // This is the route we will send GET requests to retrieve our most recent search data.
 // We will call this route the moment our page gets rendered
-app.get("/api", function(req, res) {
+app.get("/api", function(req, res) { });
 
 
 // This is the route we will send POST requests to save each search.
-app.post("/api", function(req, res) {
+app.post("/api", function(req, res) { });
 
 
  
